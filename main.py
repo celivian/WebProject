b5dcf85856086f649803cd3ef570344bb2d46839
shergeu <serzh.ilyukin@bk.ru>
2024-04-06 03:00:23 +0300

    Все для логина

--- main.py
+++ main.py
@@ -4,4 +4,7 @@ app = Flask(__name__)
 
 @app.route('/login', methods=['GET'])
 def login():
-    pass
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
